85434817e61ac19d09d7046c31941f4a30dbcb40
khauminhduy <khauminhduy@gmail.com>
2022-07-04 14:11:00 +0700

    initializer

---
+++ src/main/java/com/khauminhduy/module2/AverageRating.java
@@ -0,0 +1,28 @@
+package com.khauminhduy.module2;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+
+public class AverageRating {
+
+	public static void main(String[] args) {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		// @formatter:off
+		DataSource<Tuple3<Long, String, String>> movies = env.readCsvFile("ml-latest-small/movies.csv")
+				.ignoreFirstLine()
+				.parseQuotedStrings('"')
+				.ignoreInvalidLines()
+				.includeFields(true, true, true)
+				.types(Long.class, String.class, String.class);
+		
+		DataSource<Tuple2<Long, Double>> ratings = env.readCsvFile("ml-latest-small/ratings.csv")
+				.ignoreFirstLine()
+				.includeFields(false, true, true, false)
+				.types(Long.class, Double.class);
+		// @formatter:on
+
+	}
+
+}
---
+++ src/main/java/com/khauminhduy/module2/FilterMovies.java
@@ -0,0 +1,13 @@
+package com.khauminhduy.module2;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+public class FilterMovies {
+
+	public static void main(String[] args) {
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		
+		
+	}
+	
+}
